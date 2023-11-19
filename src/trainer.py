1e48cb88d37561dad8462bc2e22a06600031786d
SY <syeon980928@gmail.com>
2023-11-19 15:56:49 +0100

    add performance metric function in Trainer class

--- src/trainer.py
+++ src/trainer.py
@@ -17,86 +17,98 @@ class Trainer:
     def train(self, epochs):
         self.model.train()
         pbar = tqdm(total=epochs, desc='Training', leave=True)
-        history = {'train_loss': [], 'val_loss': [], 'val_accuracy': [], 'val_roc_auc': [], 'val_pr_auc': []}
+        history = {'train_loss': [], 'train_accuracy': [], 'train_roc_auc': [], 'train_pr_auc': [], 'val_loss': [],
+                   'val_accuracy': [], 'val_roc_auc': [], 'val_pr_auc': []}
 
+        # iterate over epochs
         for epoch in range(epochs):
-            total_loss = 0
-            for inputs, labels in self.train_loader:
-                inputs, labels = inputs.to(self.device), labels.to(self.device)
+            total_loss_train = 0
+            predicted_labels_train = []
+            true_labels_train = []
 
+            # iterate over batches
+            for batch, labels in self.train_loader:
+                batch, labels = batch.to(self.device), labels.to(self.device)
+
+                # traom
                 self.optimizer.zero_grad()
-                outputs = self.model(inputs)
+                outputs = self.model(batch)
                 loss = self.criterion(outputs, labels)
                 loss.backward()
                 self.optimizer.step()
 
-                total_loss += loss.item()
+                total_loss_train += loss.item()
 
-            avg_loss = total_loss / len(self.train_loader)
-            history['train_loss'].append(avg_loss)
+            # Calculate evaluation metrics from Training phase in current epoch
+            avg_loss_train = total_loss_train / len(self.train_loader)
+            history['train_loss'].append(avg_loss_train)
+            train_accuracy, train_roc_auc, train_pr_auc = self.performance_metrics(predicted_labels_train,
+                                                                                   true_labels_train)
+            history['train_accuracy'].append(train_accuracy)
+            history['train_roc_auc'].append(train_roc_auc)
+            history['train_pr_auc'].append(train_pr_auc)
 
-            # Retrieve values from validation phase in current epoch
-            val_loss, val_accuracy, val_roc_auc, val_pr_auc = self.validate(epoch)
+            # Retrieve evaluation metrics from validation phase in current epoch
+            val_loss, val_accuracy, val_roc_auc, val_pr_auc = self.evaluate(self.valid_loader)
             history['val_loss'].append(val_loss)
             history['val_accuracy'].append(val_accuracy)
             history['val_roc_auc'].append(val_roc_auc)
             history['val_pr_auc'].append(val_pr_auc)
 
             # Update progress bar after each epoch
-            pbar.set_postfix({'epoch': epoch + 1, 'training loss': avg_loss, 'validation loss': val_loss})
+            pbar.set_postfix({'epoch': epoch + 1, 'training loss': avg_loss_train, 'validation loss': val_loss})
             pbar.update(1)
 
         pbar.close()
 
         return history
 
-    def validate(self, epoch):
+    def evaluate(self, dataloader):
         self.model.eval()
-        total_val_loss = 0
-        all_outputs = []
-        all_labels = []
+        total_loss = 0
+        predicted_labels = []
+        true_labels = []
 
+        # iterate over batches
         with torch.no_grad():
-            for inputs, labels in self.valid_loader:
-                inputs, labels = inputs.to(self.device), labels.to(self.device)
-                outputs = self.model(inputs)
+            for batch, labels in dataloader:
+                batch, labels = batch.to(self.device), labels.to(self.device)
+                outputs = self.model(batch)
 
-                loss = self.criterion(outputs, labels)  # Assuming you have a criterion for validation loss
-                total_val_loss += loss.item()
+                loss = self.criterion(outputs, labels)
+                total_loss += loss.item()
 
                 # Apply sigmoid to convert to probabilities
                 probabilities = torch.sigmoid(outputs)
 
-                all_outputs.append(probabilities.cpu().numpy())
-                all_labels.append(labels.cpu().numpy())
+                predicted_labels.append(probabilities.cpu().numpy())
+                true_labels.append(labels.cpu().numpy())
 
-        avg_val_loss = total_val_loss / len(self.valid_loader)
+        # calculate performance metrics
+        avg_loss = total_loss / len(dataloader)
+        accuracy, roc_auc, pr_auc = self.performance_metrics(predicted_labels, true_labels)
 
+        return avg_loss, accuracy, roc_auc, pr_auc
+
+    # Implement function to retrieve metrics from predicted and true labels
+    def performance_metrics(self, predicted_labels, true_labels):
         # Convert lists to numpy arrays
-        all_outputs = np.concatenate(all_outputs)
-        all_labels = np.concatenate(all_labels)
+        predicted_labels = np.concatenate(predicted_labels)
+        true_labels = np.concatenate(true_labels)
 
         # Threshold for multi-label accuracy
         threshold = 0.5
-        predicted = (all_outputs > threshold).astype(int)
+        predicted = (predicted_labels > threshold).astype(int)
 
         # Multi-label accuracy
-        accuracy = np.mean(np.all(predicted == all_labels, axis=1))
+        accuracy = np.mean(np.all(predicted == true_labels, axis=1))
 
         # Calculate performance metrics
-        # use ONE vs ONE approach for ROC AUC as papers suggest to handle class imbalance better
-        roc_auc = roc_auc_score(all_labels, all_outputs, average='macro', multi_class='ovo')
-        pr_auc = average_precision_score(all_labels, all_outputs, average='macro')
-        # precision, recall, _ = precision_recall_curve(all_labels.ravel(), all_outputs.ravel())
+        # use averaging across labels for ROC AUC, PR AUC
+        roc_auc = roc_auc_score(true_labels, predicted_labels, average='macro', multi_class='ovo')
+        pr_auc = average_precision_score(true_labels, predicted_labels, average='macro')
 
-        return avg_val_loss, accuracy, roc_auc, pr_auc
-
-    def evaluate(self, test_loader):
-        return None
-
-    # Implement function to retrieve metrics from predicted and true labels
-    # (same for train, val, and evaluate as all based on predicted and true labels)
-    # def ...
+        return accuracy, roc_auc, pr_auc
 
     def save_model(self, path):
         torch.save(self.model.state_dict(), path)
