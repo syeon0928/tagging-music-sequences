045429bd57628920dcb8a517db9d2ad0b0ca29fd
Alexander Büscher <alibue@hotmail.de>
2023-11-19 14:34:04 +0100

    Implement train class in SY notebook. Allow models to accept num_classes parameter)

--- src/model_alex.py
+++ src/model_alex.py
@@ -5,9 +5,9 @@ import torch.nn.functional as F
 
 # Architecture similar to this paper:
 # Evaluation of CNN-based Automatic Music Tagging Models (arXiv:2006.00751)
-class FullyConvNet(nn.Module):
-    def __init__(self):
-        super(FullyConvNet, self).__init__()
+class FullyConvNet4(nn.Module):
+    def __init__(self, num_classes=50):
+        super(FullyConvNet4, self).__init__()
 
         # Define convolutional layers
         self.conv1 = nn.Conv2d(1, 16, kernel_size=(3, 3), padding=1)
@@ -23,13 +23,52 @@ class FullyConvNet(nn.Module):
 
         # The final convolutional layer to adjust the output to the desired number of classes
         # Note: The number of output features needs to be tuned based on the final output size after the last pooling layer
-        self.final_conv = nn.Conv2d(128, 50, kernel_size=(1, 1))
+        self.final_conv = nn.Conv2d(128, num_classes, kernel_size=(1, 1))
 
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = self.pool3(F.relu(self.conv3(x)))
         x = self.pool4(F.relu(self.conv4(x)))
+
+        # Apply final convolution to get to the desired label shape (batch_size, 50, 1, 1)
+        x = self.final_conv(x)
+
+        # Flattening the output for the label shape: torch.Size([batch_size, 50])
+        x = torch.flatten(x, 1)
+
+        return x
+
+
+# Based on: Evaluation of CNN-based Automatic Music Tagging Models (arXiv:2006.00751)
+class FullyConvNet5(nn.Module):
+    def __init__(self, num_classes=50):
+        super(FullyConvNet5, self).__init__()
+
+        # Define convolutional layers with the specified number of feature maps
+        self.conv1 = nn.Conv2d(1, 128, kernel_size=(3, 3), padding=1)
+        self.conv2 = nn.Conv2d(128, 256, kernel_size=(3, 3), padding=1)
+        self.conv3 = nn.Conv2d(256, 512, kernel_size=(3, 3), padding=1)
+        self.conv4 = nn.Conv2d(512, 1024, kernel_size=(3, 3), padding=1)
+        self.conv5 = nn.Conv2d(1024, 2048, kernel_size=(3, 3), padding=1)
+
+        # Define max-pooling layers with specified strides
+        self.pool1 = nn.MaxPool2d((2, 4))
+        self.pool2 = nn.MaxPool2d((2, 4))
+        self.pool3 = nn.MaxPool2d((2, 4))
+        self.pool4 = nn.MaxPool2d((3, 5))
+        self.pool5 = nn.MaxPool2d((4, 4))
+
+        # Final convolutional layer to adjust the output to 50 dimensions
+        # Note: Kernel size should be adjusted based on the output size of the last pooling layer
+        self.final_conv = nn.Conv2d(2048, num_classes, kernel_size=(1, 1))
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.pool4(F.relu(self.conv4(x)))
+        x = self.pool5(F.relu(self.conv5(x)))
 
         # Apply final convolution to get to the desired label shape (batch_size, 50, 1, 1)
         x = self.final_conv(x)
--- src/trainer.py
+++ src/trainer.py
@@ -1,10 +1,8 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-from sklearn.metrics import roc_auc_score, precision_recall_curve, auc
 import numpy as np
+from tqdm import tqdm
+from sklearn.metrics import roc_auc_score, precision_recall_curve, auc, average_precision_score
 
 
 class Trainer:
@@ -19,6 +17,7 @@ class Trainer:
     def train(self, epochs):
         self.model.train()
         pbar = tqdm(total=epochs, desc='Training', leave=True)
+        history = {'train_loss': [], 'val_loss': [], 'val_accuracy': [], 'val_roc_auc': [], 'val_pr_auc': []}
 
         for epoch in range(epochs):
             total_loss = 0
@@ -34,18 +33,29 @@ class Trainer:
                 total_loss += loss.item()
 
             avg_loss = total_loss / len(self.train_loader)
+            history['train_loss'].append(avg_loss)
 
             # Update progress bar after each epoch
             pbar.set_postfix({'epoch': epoch + 1, 'loss': avg_loss / len(self.train_loader)})
             pbar.update(1)
+
+            # Retrieve values from validation phase in current epoch
+            val_loss, val_accuracy, val_roc_auc, val_pr_auc = self.validate(epoch)
+            history['val_loss'].append(val_loss)
+            history['val_accuracy'].append(val_accuracy)
+            history['val_roc_auc'].append(val_roc_auc)
+            history['val_pr_auc'].append(val_pr_auc)
 
             print(f'Epoch [{epoch+1}/{epochs}], Loss: {avg_loss:.4f}')
             self.validate(epoch)
 
         pbar.close()
 
+        return history
+
     def validate(self, epoch):
         self.model.eval()
+        total_val_loss = 0
         all_outputs = []
         all_labels = []
 
@@ -54,11 +64,16 @@ class Trainer:
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
                 outputs = self.model(inputs)
 
+                loss = self.criterion(outputs, labels)  # Assuming you have a criterion for validation loss
+                total_val_loss += loss.item()
+
                 # Apply sigmoid to convert to probabilities
                 probabilities = torch.sigmoid(outputs)
 
                 all_outputs.append(probabilities.cpu().numpy())
                 all_labels.append(labels.cpu().numpy())
+
+        avg_val_loss = total_val_loss / len(self.valid_loader)
 
         # Convert lists to numpy arrays
         all_outputs = np.concatenate(all_outputs)
@@ -70,15 +85,49 @@ class Trainer:
 
         # Multi-label accuracy
         accuracy = np.mean(np.all(predicted == all_labels, axis=1))
-        print(f'Epoch [{epoch + 1}], Validation Accuracy: {accuracy:.2f}%')
 
-        # ROC AUC and AUC PR calculations
-        # Note: These metrics might not be completely informative for imbalanced multi-label datasets
+        # Calculate performance metrics
+        # use ONE vs ONE approach for ROC AUC as papers suggest to handle class imbalance better
+        roc_auc = roc_auc_score(all_labels, all_outputs, average='macro', multi_class='ovo')
+        pr_auc = average_precision_score(all_labels, all_outputs, average='macro')
+        precision, recall, _ = precision_recall_curve(all_labels.ravel(), all_outputs.ravel())
+
+        print(f'Epoch [{epoch + 1}] Validation Metrics: \n, Validation Accuracy: {accuracy:.2f}%, ROC AUC: {roc_auc:.2f}, PR AUC: {pr_auc:.2f}')
+
+        return avg_val_loss, accuracy, roc_auc, pr_auc
+
+    def evaluate(self, test_loader):
+        self.model.eval()
+        all_outputs = []
+        all_labels = []
+
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                outputs = self.model(inputs)
+                probabilities = torch.sigmoid(outputs)
+                all_outputs.append(probabilities.cpu().numpy())
+                all_labels.append(labels.cpu().numpy())
+
+        all_outputs = np.concatenate(all_outputs)
+        all_labels = np.concatenate(all_labels)
+
+        threshold = 0.5
+        predicted = (all_outputs > threshold).astype(int)
+        accuracy = np.mean(np.all(predicted == all_labels, axis=1))
+
         roc_auc = roc_auc_score(all_labels, all_outputs, average='macro', multi_class='ovr')
         precision, recall, _ = precision_recall_curve(all_labels.ravel(), all_outputs.ravel())
         auc_pr = auc(recall, precision)
 
-        print(f'ROC AUC: {roc_auc:.2f}, AUC PR: {auc_pr:.2f}')
+        # Return a dictionary of metrics
+        return {
+            'accuracy': accuracy,
+            'roc_auc': roc_auc,
+            'auc_pr': auc_pr,
+            'all_outputs': all_outputs,
+            'all_labels': all_labels
+        }
 
     def save_model(self, path):
         torch.save(self.model.state_dict(), path)
