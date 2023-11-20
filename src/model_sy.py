3da42d21f7802e0b00d61941f5a9719e0eddd56b
Seungyeon Han <syeon980928@gmail.com>
2023-11-20 16:15:25 +0100

    for testing model architecture only in hpc

--- src/model_sy.py
+++ src/model_sy.py
@@ -18,101 +18,101 @@ from tqdm import tqdm
 import torch.nn.functional as F
 import numpy as np
 
-#
-# class WaveCNN9(nn.Module):
-#     def __init__(self, num_classes=50):
-#         super(WaveCNN9, self).__init__()
-#
-#         # Convolutional blocks
-#         self.conv_blocks = nn.ModuleList()
-#         in_channels = 1
-#         out_channels = 128
-#         for i in range(9):
-#
-#             if i == 4:  # 5th layer
-#                 out_channels = 256
-#             if i == 8:  # Last layer
-#                 out_channels = 512
-#
-#             self.conv_blocks.append(nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=1))
-#             self.conv_blocks.append(nn.BatchNorm1d(out_channels))
-#             self.conv_blocks.append(nn.ReLU())
-#             self.conv_blocks.append(nn.MaxPool1d(kernel_size=3, stride=3))
-#             in_channels = out_channels
-#
-#             # Global max pooling
-#         self.global_max_pool = nn.AdaptiveMaxPool1d(1)
-#
-#         # Fully connected layers
-#         self.fc1 = nn.Linear(512, 256)
-#         self.fc2 = nn.Linear(256, num_classes)
-#
-#     def forward(self, x):
-#         # Convolutional blocks
-#         for block in self.conv_blocks:
-#             x = block(x)
-#
-#         # Global max pooling
-#         x = self.global_max_pool(x)
-#         x = x.view(x.size(0), -1)  # Flatten
-#
-#         # Fully connected layers
-#         x = F.relu(self.fc1(x))
-#         x = self.fc2(x)
-#
-#         return x
-#
-#
-# class WaveCNN7(nn.Module):
-#     def __init__(self, num_classes=50):
-#         super(WaveCNN7, self).__init__()
-#
-#         # Strided convolution to reduce dimensionality
-#         self.strided_conv = nn.Conv1d(1, 128, kernel_size=3, stride=3)
-#         self.bn0 = nn.BatchNorm1d(128)
-#
-#         # Convolutional blocks
-#         self.conv_blocks = nn.ModuleList()
-#         in_channels = 128
-#         out_channels = 128
-#         for i in range(6):
-#
-#             if i == 2:  # 3rd layer
-#                 out_channels = 256
-#             if i == 5:  # Last layer
-#                 out_channels = 512
-#
-#             self.conv_blocks.append(nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=1))
-#             self.conv_blocks.append(nn.BatchNorm1d(out_channels))
-#             self.conv_blocks.append(nn.ReLU())
-#             self.conv_blocks.append(nn.MaxPool1d(kernel_size=3, stride=3))
-#             in_channels = out_channels
-#
-#         # Global max pooling
-#         self.global_max_pool = nn.AdaptiveMaxPool1d(1)
-#
-#         # Fully connected layers
-#         # Adjust the input size of the first FC layer based on the output of the last convolutional block
-#         self.fc1 = nn.Linear(512, 256)  # Adjust 128 based on the output channels of the last conv block
-#         self.fc2 = nn.Linear(256, num_classes)
-#
-#     def forward(self, x):
-#         # Initial strided convolution
-#         x = F.relu(self.bn0(self.strided_conv(x)))
-#
-#         # Convolutional blocks
-#         for block in self.conv_blocks:
-#             x = block(x)
-#
-#         # Global max pooling
-#         x = self.global_max_pool(x)
-#         x = x.view(x.size(0), -1)  # Flatten
-#
-#         # Fully connected layers
-#         x = F.relu(self.fc1(x))
-#         x = self.fc2(x)
-#
-#         return x
+
+class WaveCNN9(nn.Module):
+    def __init__(self, num_classes=50):
+        super(WaveCNN9, self).__init__()
+
+        # Convolutional blocks
+        self.conv_blocks = nn.ModuleList()
+        in_channels = 1
+        out_channels = 128
+        for i in range(9):
+
+            if i == 4:  # 5th layer
+                out_channels = 256
+            if i == 8:  # Last layer
+                out_channels = 512
+
+            self.conv_blocks.append(nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=1))
+            self.conv_blocks.append(nn.BatchNorm1d(out_channels))
+            self.conv_blocks.append(nn.ReLU())
+            self.conv_blocks.append(nn.MaxPool1d(kernel_size=3, stride=3))
+            in_channels = out_channels
+
+            # Global max pooling
+        self.global_max_pool = nn.AdaptiveMaxPool1d(1)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        # Convolutional blocks
+        for block in self.conv_blocks:
+            x = block(x)
+
+        # Global max pooling
+        x = self.global_max_pool(x)
+        x = x.view(x.size(0), -1)  # Flatten
+
+        # Fully connected layers
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+
+class WaveCNN7(nn.Module):
+    def __init__(self, num_classes=50):
+        super(WaveCNN7, self).__init__()
+
+        # Strided convolution to reduce dimensionality
+        self.strided_conv = nn.Conv1d(1, 128, kernel_size=3, stride=3)
+        self.bn0 = nn.BatchNorm1d(128)
+
+        # Convolutional blocks
+        self.conv_blocks = nn.ModuleList()
+        in_channels = 128
+        out_channels = 128
+        for i in range(6):
+
+            if i == 2:  # 3rd layer
+                out_channels = 256
+            if i == 5:  # Last layer
+                out_channels = 512
+
+            self.conv_blocks.append(nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=1))
+            self.conv_blocks.append(nn.BatchNorm1d(out_channels))
+            self.conv_blocks.append(nn.ReLU())
+            self.conv_blocks.append(nn.MaxPool1d(kernel_size=3, stride=3))
+            in_channels = out_channels
+
+        # Global max pooling
+        self.global_max_pool = nn.AdaptiveMaxPool1d(1)
+
+        # Fully connected layers
+        # Adjust the input size of the first FC layer based on the output of the last convolutional block
+        self.fc1 = nn.Linear(512, 256)  # Adjust 128 based on the output channels of the last conv block
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        # Initial strided convolution
+        x = F.relu(self.bn0(self.strided_conv(x)))
+
+        # Convolutional blocks
+        for block in self.conv_blocks:
+            x = block(x)
+
+        # Global max pooling
+        x = self.global_max_pool(x)
+        x = x.view(x.size(0), -1)  # Flatten
+
+        # Fully connected layers
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
 
 
 class WaveCNN5(nn.Module):
@@ -214,11 +214,11 @@ if __name__ == '__main__':
 
     ### CNN 9
     # # # check the summary
-    # print('Start Training WaveCNN 7 Layers')
-    # wavecnn9 = WaveCNN9(num_classes=50)
-    # input_size = (train_features.size()[1:])
-    # model_summary = summary(wavecnn9.to(device), input_size) if device == 'cuda' else summary(wavecnn9, input_size)
-    # print(model_summary)
+    print('Start Training WaveCNN 9 Layers')
+    wavecnn9 = WaveCNN9(num_classes=50)
+    input_size = (train_features.size()[1:])
+    model_summary = summary(wavecnn9.to(device), input_size) if device == 'cuda' else summary(wavecnn9, input_size)
+    print(model_summary)
     #
     # # Train
     # # Instantiate trainer
@@ -226,14 +226,14 @@ if __name__ == '__main__':
     # optimizer = optim.Adam(wavecnn9.parameters(), lr=LEARNING_RATE)
     # trainer = Trainer(wavecnn9, train_dataloader, val_dataloader, criterion, optimizer, device)
     # trainer.train(epochs=EPOCHS)
-    #
-    # ### CNN 7
-    # # check the summary
-    # print('Start Training WaveCNN 7 Layers')
-    # wavecnn7 = WaveCNN7(num_classes=50)
-    # input_size = (train_features.size()[1:])
-    # model_summary = summary(wavecnn7.to(device), input_size) if device == 'cuda' else summary(wavecnn7, input_size)
-    # print(model_summary)
+
+    ### CNN 7
+    # check the summary
+    print('Start Training WaveCNN 7 Layers')
+    wavecnn7 = WaveCNN7(num_classes=50)
+    input_size = (train_features.size()[1:])
+    model_summary = summary(wavecnn7.to(device), input_size) if device == 'cuda' else summary(wavecnn7, input_size)
+    print(model_summary)
     #
     # # Train
     # # Instantiate trainer
@@ -253,11 +253,11 @@ if __name__ == '__main__':
     model_summary = summary(wavecnn5.to(device), input_size) if device == 'cuda' else summary(wavecnn5, input_size)
     print(model_summary)
 
-    # Train
-    # Instantiate trainer
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(wavecnn5.parameters(), lr=LEARNING_RATE)
-    traine_5 = Trainer(wavecnn5, train_dataloader, val_dataloader, criterion, optimizer, device)
-
-    traine_5.train(epochs=EPOCHS)
-    traine_5.save_model('../models/waveform_cnn5.pth')
+    # # Train
+    # # Instantiate trainer
+    # criterion = nn.BCEWithLogitsLoss()
+    # optimizer = optim.Adam(wavecnn5.parameters(), lr=LEARNING_RATE)
+    # traine_5 = Trainer(wavecnn5, train_dataloader, val_dataloader, criterion, optimizer, device)
+    #
+    # traine_5.train(epochs=EPOCHS)
+    # traine_5.save_model('../models/waveform_cnn5.pth')
