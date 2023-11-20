fdf188246f6b4321bb56462e421ea828213594c0
Alexander <alibue@hotmail.de>
2023-11-20 17:43:36 +0100

    update sbatch script

--- scripts/run_model_training_sy.sh
+++ scripts/run_model_training_sy.sh
@@ -2,7 +2,8 @@
 #SBATCH --job-name train-model
 #SBATCH --output=train-model.%j.out                # Name of output file (%j expands to jobId)
 #SBATCH --gres=gpu
-#SBATCH --cpus-per-task=32        # Schedule 32 cores (idk what im doing)
+#SBATCH --cpus-per-task=16        
+#SBATCH --mem=64G
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --time 8:00:00
