2cee934d55fcbf22c8fcc34a71ae55533090fb83
Alexander <alibue@hotmail.de>
2023-11-22 10:04:25 +0100

    Fix bool flag parsing

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name training
-#SBATCH --output=train.%j.out                # Name of output file (%j expands to jobId)
+#SBATCH --output=scripts/training-%j.out                # Name of output file (%j expands to jobId)
 #SBATCH --gres=gpu
 #SBATCH --cpus-per-task=16        
 #SBATCH --mem=64G
@@ -9,7 +9,7 @@
 #SBATCH --time 16:00:00
 #SBATCH --partition brown                   
 #SBATCH --mail-type=BEGIN,END,FAIL               # When to send email
-#SBATCH --mail-user=$USER@itu.dk            # Email to which to send updates
+#SBATCH --mail-user=abys@itu.dk           # Email to which to send updates
 
 # load modules or conda environments here
 # update the $CONDAENV here to match the environment created earlier
@@ -17,4 +17,4 @@ module load Anaconda3
 source activate aml-project
 
 # Run script
-python train.py --apply_transformations=True --apply_augmentations=False
+python train.py --shuffle-train --num_workers=16 --apply_transformations --apply_augmentations
