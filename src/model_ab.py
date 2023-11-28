26d22a29ceb17c0a0675ec4e9fff74a7752ad7f7
Alexander Büscher <alibue@hotmail.de>
2023-11-28 23:22:05 +0100

    Update model architecture FCN4

--- src/model_ab.py
+++ src/model_ab.py
@@ -9,21 +9,19 @@ class FullyConvNet4(nn.Module):
     def __init__(self, num_classes=50):
         super(FullyConvNet4, self).__init__()
 
-        # Define convolutional layers
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=(3, 3), padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=(3, 3), padding=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3), padding=1)
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=(3, 3), padding=1)
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(1, 128, kernel_size=(3, 3), padding=1)
+        self.conv2 = nn.Conv2d(128, 384, kernel_size=(3, 3), padding=1)
+        self.conv3 = nn.Conv2d(384, 768, kernel_size=(3, 3), padding=1)
+        self.conv4 = nn.Conv2d(768, 2048, kernel_size=(3, 3), padding=1)
 
-        # Define max-pooling layers with different strides
+        # Max-pooling layers
         self.pool1 = nn.MaxPool2d((2, 4))
         self.pool2 = nn.MaxPool2d((4, 5))
         self.pool3 = nn.MaxPool2d((3, 8))
         self.pool4 = nn.MaxPool2d((4, 8))
 
-        # The final convolutional layer to adjust the output to the desired number of classes
-        # Note: The number of output features needs to be tuned based on the final output size after the last pooling layer
-        self.final_conv = nn.Conv2d(128, num_classes, kernel_size=(1, 1))
+        self.final_conv = nn.Conv2d(2048, num_classes, kernel_size=(1, 1))
 
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
@@ -31,11 +29,14 @@ class FullyConvNet4(nn.Module):
         x = self.pool3(F.relu(self.conv3(x)))
         x = self.pool4(F.relu(self.conv4(x)))
 
-        # Apply final convolution to get to the desired label shape (batch_size, 50, 1, 1)
+        # Apply final convolution to get to the desired label shape (batch_size, num_classes, 1, 1)
         x = self.final_conv(x)
 
-        # Flattening the output for the label shape: torch.Size([batch_size, 50])
+        # Flattening the output for the label shape: torch.Size([batch_size, num_classes])
         x = torch.flatten(x, 1)
+
+        # Apply sigmoid activation function
+        x = torch.sigmoid(x)
 
         return x
 
