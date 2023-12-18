ede2a70c9c545918bc0f8cc549e4eb6d04ba29ca
Alexander <alibue@hotmail.de>
2023-12-18 23:49:57 +0100

    refactor: move some models to archive

--- train.sh
+++ train.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #SBATCH --job-name training
-#SBATCH --output=scripts/model-%j.out                # Name of output file (%j expands to jobId)
+#SBATCH --output=logs/all-fcn-musiccnn-%j.out                # Name of output file (%j expands to jobId)
 #SBATCH --gres=gpu
 #SBATCH --cpus-per-task=16        
 #SBATCH --mem=32G
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
-#SBATCH --time 24:00:00
+#SBATCH --time 16:00:00
 #SBATCH --partition brown                   
 #SBATCH --mail-type=BEGIN,END,FAIL               # When to send email
-#SBATCH --mail-user=seuh@itu.dk           # Email to which to send updates
+#SBATCH --mail-user=abys@itu.dk           # Email to which to send updates
 
 # Activate environment
 module load Anaconda3
 source activate aml-project
 
 # Run script
-python train.py --model_class_name="FCN7_Transfer" --num_workers=16 --train_annotations='gtzan_train_label.csv' --val_annotations='gtzan_val_label.csv' --test_annotations='gtzan_test_label.csv'
+python train.py --model_class_name="FCN3" --num_workers=16 --apply_transformations
+python train.py --model_class_name="FCN4" --num_workers=16 --apply_transformations
+python train.py --model_class_name="FCN5" --num_workers=16 --apply_transformations
+python train.py --model_class_name="MusicCNN" --num_workers=16 --apply_transformations
